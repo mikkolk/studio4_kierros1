13fefd84c23a601add25a943c3d204e83c104776
Mikko Latva-Kayra <mikko.latva-kayra@aalto.fi>
2012-09-30 18:23:24 +0300

    fixed processing libraries

--- src/Visualisaatio.java
+++ src/Visualisaatio.java
@@ -1,4 +1,17 @@
 
-public class Visualisaatio {
+import processing.core.*;
 
+public class Visualisaatio extends PApplet {
+
+  public void setup() {
+    size(200,200);
+    background(0);
+  }
+
+  public void draw() {
+    stroke(255);
+    if (mousePressed) {
+      line(mouseX,mouseY,pmouseX,pmouseY);
+    }
+  }
 }
