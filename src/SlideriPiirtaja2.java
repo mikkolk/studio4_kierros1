22442a6275ddbf49933702cca4dcd79056f12a87
Mikko Latva-Kayra <mikko.latva-kayra@aalto.fi>
2012-10-10 12:49:09 +0300

    added custom slider. .png:s still need work

--- src/SlideriPiirtaja2.java
+++ src/SlideriPiirtaja2.java
@@ -16,7 +16,8 @@ public class SlideriPiirtaja2 {
     public SlideriPiirtaja2(Visualisaatio pohja){
     	this.slider2Arvo = 2012;
         this.pohja = pohja;
-        this.slider2 = new GWSlider(this.pohja, (int) (0.2*(this.annaPohja().width)),
+        this.slider2 = new GWSlider(this.pohja, "omaSlider", 
+                (int) (0.2*(this.annaPohja().width)),
                 (int) (0.97*(this.annaPohja().height)),
                 (int) (0.6*(this.annaPohja().width)));
         this.slider2.setValueType(GWSlider.INTEGER);
